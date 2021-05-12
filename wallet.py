a691cc87e1d74f9d0c5488b88cd13677e59ae069
Rahul Shah <rahulshah512@gmail.com>
2021-05-12 10:32:07 -0700

    code cleanup

--- wallet.py
+++ wallet.py
@@ -28,7 +28,7 @@ PEER_NODES = ["http://localhost:5002", "http://localhost:5001", "http://localhos
 def wallet():
     response = None
     while response not in ["1", "2", "3"]:
-        response = input("""What do you want to do?
+        response = input("""Please choose an option from below
         1. Generate new wallet
         2. Send coins to another wallet
         3. View Blockchain\n""")
@@ -37,32 +37,32 @@ def wallet():
         print("""=========================================\n
 IMPORTANT: save this credentials or you won't be able to recover your wallet\n
 =========================================\n""")
-        generate_ECDSA_keys()
+        create_wallet()
     elif response == "2":
-        addr_from = input("From: introduce your wallet address (public key)\n")
-        private_key = input("Introduce your private key\n")
-        addr_to = input("To: introduce destination wallet address\n")
-        amount = input("Amount: number stating how much do you want to send\n")
+        addr_from = input("From: Enter your wallet address (public key)\n")
+        private_key = input("Enter your wallet's private key\n")
+        addr_to = input("To: Enter the destination wallet's address\n")
+        amount = input("Amount: How many ShahCoin's do you want to send\n")
         print("=========================================\n\n")
         print("Is everything correct?\n")
         print("From: {0}\nPrivate Key: {1}\nTo: {2}\nAmount: {3}\n".format(addr_from, private_key, addr_to, amount))
         response = input("y/n\n")
         if response.lower() == "y":
             send_transaction(addr_from, private_key, addr_to, amount)
-    else:  # Will always occur when response == 3.
+    elif response == "3":  # Will always occur when response == 3.
         view_blockchain()
+
+
 
 
 def send_transaction(addr_from, private_key, addr_to, amount):
     """Sends your transaction to different nodes. Once any of the nodes manage
-    to mine a block, your transaction will be added to the blockchain. Despite
-    that, there is a low chance your transaction gets canceled due to other nodes
-    having a longer chain. So make sure your transaction is deep into the chain
-    before claiming it as approved!
+    to mine a block, your transaction will be added to the blockchain. There is a
+    chance your transaction will be dropped if another node has a longer chain.
     """
 
     if len(private_key) == 64:
-        signature, message = sign_ECDSA_msg(private_key)
+        signature, message = sign_timestamp_ECDSA(private_key)
         payload = {"from": addr_from,
                    "to": addr_to,
                    "amount": amount,
@@ -100,7 +100,7 @@ def view_blockchain():
     print("Blockchain Length: ", max_blockchain_length)
     print("-----------------------------------")
     print(blockchain)
-def generate_ECDSA_keys():
+def create_wallet():
     """This function takes care of creating your private and public (your address) keys.
     It's very important you don't lose any of them or those wallets will be lost
     forever. If someone else get access to your private key, you risk losing your coins.
@@ -120,8 +120,8 @@ def generate_ECDSA_keys():
         f.write("Private key: {0}\nWallet address / Public key: {1}".format(private_key, public_key.decode()))
     print("Your new address and private key are now in the file {0}".format(filename))
 
-def sign_ECDSA_msg(private_key):
-    """Sign the message to be sent
+def sign_timestamp_ECDSA(private_key):
+    """Sign the transaction timestamp
     private_key: must be hex
 
     return
@@ -129,11 +129,11 @@ def sign_ECDSA_msg(private_key):
     message: str
     """
     # Get timestamp, round it, make it into a string and encode it to bytes
-    message = str(round(time.time()))
-    bmessage = message.encode()
+    timestamp = str(round(time.time()))
+    bmessage = timestamp.encode()
     sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.SECP256k1)
     signature = base64.b64encode(sk.sign(bmessage))
-    return signature, message
+    return signature, timestamp
 
 
 if __name__ == '__main__':
